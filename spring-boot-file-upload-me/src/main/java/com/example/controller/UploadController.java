da5bfa5d057b6a1fa7fc501942f1fe65e64dfa98
“lingyun666” <873983080@qq.com>
2021-05-07 10:57:11 +0800

    Add 后台上传文件

--- spring-boot-file-upload-me/src/main/java/com/example/controller/UploadController.java
+++ spring-boot-file-upload-me/src/main/java/com/example/controller/UploadController.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -13,6 +18,8 @@ import java.util.List;
  */
 @Controller
 public class UploadController {
+    // 指定文件输出路径
+    private static String UPLOAD_FOLDER = "E://temp//";
     /**
      * 重定向到"upload.html"
      *
@@ -30,9 +37,20 @@ public class UploadController {
      * @return
      */
     @PostMapping("upload/one")
-    public String uploadImage(@RequestParam("mf") MultipartFile multipartFile) {
+    @ResponseBody
+    public String uploadImage(@RequestParam("mf") MultipartFile multipartFile) throws IOException {
         System.out.println(multipartFile);
         System.out.println("==============");
+        // 后台上传文件至系统的指定文件夹
+        byte[] bytes = multipartFile.getBytes();//获取MultipartFile文字为二进制数据流
+        Path dir = Paths.get(UPLOAD_FOLDER);
+        Path path = Paths.get(UPLOAD_FOLDER + multipartFile.getOriginalFilename());
+        // 判断该目录是否存在,不存在,则创建一个
+        if (!Files.exists(dir)) {
+            Files.createDirectories(dir);
+        }
+        // 将bytes数据写进指令路径下
+        Files.write(path, bytes);
         return "success";
     }
 
@@ -42,6 +60,7 @@ public class UploadController {
      * @return
      */
     @PostMapping("upload/many")
+    @ResponseBody
     public String uploadMany(@RequestParam("mfs") List<MultipartFile> multipartFiles) {
         System.out.println(multipartFiles);
         System.out.println("******************");
