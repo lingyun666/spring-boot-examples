d3c75c4b0539f7c714e936d6dc843d7c70b71ffc
“lingyun666” <873983080@qq.com>
2021-05-13 08:41:54 +0800

    U

--- spring-boot-file-upload-me/src/main/java/com/example/controller/UploadController.java
+++ spring-boot-file-upload-me/src/main/java/com/example/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,6 +22,7 @@ import java.util.List;
 public class UploadController {
     // 指定文件输出路径
     private static String UPLOAD_FOLDER = "E://temp//";
+
     /**
      * 重定向到"upload.html"
      */
@@ -36,6 +39,29 @@ public class UploadController {
     public String uploadImage(@RequestParam("mf") MultipartFile multipartFile) throws IOException {
         System.out.println(multipartFile);
         System.out.println("==============");
+        upload(multipartFile);
+        testMethod(multipartFile);
+
+        return "success";
+    }
+
+    private void testMethod(MultipartFile mf) throws IOException {
+        String a = mf.getName();//
+        String b = mf.getOriginalFilename();//文件名: 1.png
+        String c = mf.getContentType();//内容类型: image/png(图片/png格式)
+        long siz = mf.getSize();//文件大小: 646482
+        Resource r = mf.getResource();//获取资源: MultipartFile resource [mf]
+        try {
+            byte[] bytes = mf.getBytes();//获取byte数组类型的资源(需要异常处理)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file = new File("E://temp//han//" + b);
+        mf.transferTo(file);
+    }
+
+    private void upload(MultipartFile multipartFile) throws IOException {
         // 后台上传文件至系统的指定文件夹
         byte[] bytes = multipartFile.getBytes();//获取MultipartFile文字为二进制数据流
         Path dir = Paths.get(UPLOAD_FOLDER);
@@ -46,11 +72,11 @@ public class UploadController {
         }
         // 将bytes数据写进指令路径下
         Files.write(path, bytes);
-        return "success";
     }
 
     /**
      * 接收多个文件(不能接收视频)
+     *
      * @param multipartFiles
      * @return
      */
--- spring-boot-file-upload-me/src/test/java/com/example/SpringBootFileUploadMeApplicationTests.java
+++ spring-boot-file-upload-me/src/test/java/com/example/SpringBootFileUploadMeApplicationTests.java
@@ -2,12 +2,28 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.UrlResource;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootTest
 class SpringBootFileUploadMeApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws MalformedURLException {
+        // att: 将String类型"storage"转为Path类
+        Path path1 = Paths.get("storage");
+        // att: 调用Path类,继续解析路径
+        Path path2 = path1.resolve("a.png");
+        // att: 返回当前path的uri(jdk8里的类)
+        URI uri1 = path2.toUri();
+        // att: 将uri资源转为url
+        UrlResource urlResource = new UrlResource(uri1);
+
+        System.out.println(urlResource.toString());
     }
 
 }
